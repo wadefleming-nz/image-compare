d56a6ee67bcfaf3ac500e71f01918e53f51a533c
Wade Fleming <wadefleming2000@hotmail.com>
2020-12-28 18:08:48 +0200

    better management of object URLs

--- src/components/ImageComparer.tsx
+++ src/components/ImageComparer.tsx
@@ -3,33 +3,40 @@ import { ImageComparison } from './ImageComparison';
 import { ImageFileInput } from './ImageFileInput';
 
 type ImageComparerState = {
-  beforeImageFile: File | undefined;
-  afterImageFile: File | undefined;
+  beforeImageUrl: string;
+  afterImageUrl: string;
 };
 
 export class ImageComparer extends React.Component<{}, ImageComparerState> {
   constructor(props: never) {
     super(props);
     this.state = {
-      beforeImageFile: undefined,
-      afterImageFile: undefined,
+      beforeImageUrl: '',
+      afterImageUrl: '',
     };
   }
 
-  onBeforeFileSelected = (file: File | undefined) =>
-    this.setState({ beforeImageFile: file });
-  onAfterFileSelected = (file: File | undefined) =>
-    this.setState({ afterImageFile: file });
+  onBeforeFileSelected = (file: File | undefined) => {
+    if (this.state.beforeImageUrl) {
+      URL.revokeObjectURL(this.state.beforeImageUrl);
+    }
+    this.setState({ beforeImageUrl: URL.createObjectURL(file) });
+  };
+
+  onAfterFileSelected = (file: File | undefined) => {
+    if (this.state.afterImageUrl) {
+      URL.revokeObjectURL(this.state.afterImageUrl);
+    }
+    this.setState({ afterImageUrl: URL.createObjectURL(file) });
+  };
 
   render() {
     return (
       <div>
         <ImageFileInput onFileSelected={this.onBeforeFileSelected} />
         <ImageFileInput onFileSelected={this.onAfterFileSelected} />
-        {this.state.beforeImageFile ? (
-          <ImageComparison
-            src={URL.createObjectURL(this.state.beforeImageFile)}
-          />
+        {this.state.beforeImageUrl ? (
+          <ImageComparison src={this.state.beforeImageUrl} />
         ) : (
           <div>Please select files</div>
         )}
