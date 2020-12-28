615a3204e6c3832e2ade6147ab1cf1cc615cf120
Wade Fleming <wadefleming2000@hotmail.com>
2020-12-28 16:23:58 +0200

    added example placeholder image

--- src/components/ImageComparison.tsx
+++ src/components/ImageComparison.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import beforeImage from '../assets/example-image-before.jpg';
 
 export class ImageComparison extends React.Component {
   render() {
-    return <div>Image Comparison</div>;
+    return <img src={beforeImage} alt="Image for Comparison" />;
   }
 }
