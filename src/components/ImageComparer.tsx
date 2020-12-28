52565ca2a25ecc7c6c12708ff81644eaf24cf8a8
Wade Fleming <wadefleming2000@hotmail.com>
2020-12-28 13:25:41 +0200

    added outline ImageFileInput component

--- src/components/ImageComparer.tsx
+++ src/components/ImageComparer.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { ImageFileInput } from './ImageFileInput';
 
 export class ImageComparer extends React.Component {
   render() {
-    return <div>Image Comparer</div>;
+    return (
+      <div>
+        <ImageFileInput />
+        <div>Image Comparer</div>
+      </div>
+    );
   }
 }
