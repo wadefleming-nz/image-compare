36d4a5509dd3230297becbde85be76cf8f2aaa86
Wade Fleming <wadefleming2000@hotmail.com>
2020-12-29 18:48:06 +0200

    style slider/handle

--- src/components/ImageComparison.tsx
+++ src/components/ImageComparison.tsx
@@ -9,11 +9,18 @@ type ImageComparisonProps = {
 
 export class ImageComparison extends React.Component<ImageComparisonProps> {
   render() {
+    const offWhite = 'rgb(240,240,240)';
+    const greyTransparent = 'rgba(180, 180, 180, 0.5)';
+
     return (
       <div className="container">
         <ImageSlider
           image1={this.props.afterSrc} // slider lib puts image1 on the right
           image2={this.props.beforeSrc} // and image2 on the left
+          sliderWidth={1}
+          sliderColor={offWhite}
+          handleBackgroundColor={greyTransparent}
+          handleColor={'white'}
         />
       </div>
     );
