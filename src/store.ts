4b596f67d1afb5e16f5a1b62d24a98c410947035
Wade Fleming <wadefleming2000@hotmail.com>
2021-01-01 20:46:25 +0200

    use combineReducers

--- src/store.ts
+++ src/store.ts
@@ -1,14 +1,8 @@
-import imagesReducer, { ImagesState } from './features/image-comparer/reducer';
+import { combineReducers } from '@reduxjs/toolkit';
+import imagesReducer from './features/image-comparer/reducer';
 
-const initialState = {
-  images: null as ImagesState,
-};
+const rootReducer = combineReducers({
+  images: imagesReducer,
+});
 
-export default function rootReducer(
-  state = initialState,
-  action: { payload: any; type: string }
-) {
-  return {
-    images: imagesReducer(state.images, action),
-  };
-}
+export default rootReducer;
